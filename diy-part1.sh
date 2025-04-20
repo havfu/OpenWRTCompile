4154a0ef2def63af91e29df9b1d22da9b0a118b0
havfu <147126552+havfu@users.noreply.github.com>
2025-04-20 19:34:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,4 @@
 
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
+echo 'src-git golang https://github.com/kenzok8/golang.git;24.x' >>feeds.conf.default
